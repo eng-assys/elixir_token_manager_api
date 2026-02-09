8fec5cb2989aa045e0720f05676132fcf1307c95
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-09 15:05:20 -0300

    enhance seed to generate history and active tokens

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,4 +10,36 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-Enum.each(1..100, fn _ -> ExTokenManagerApi.Repo.insert!(%ExTokenManagerApi.Tokens.Token{}) end)
+alias ExTokenManagerApi.Repo
+alias ExTokenManagerApi.Tokens.{Token, TokenUsageHistory}
+
+Repo.delete_all(TokenUsageHistory)
+Repo.delete_all(Token)
+
+IO.puts "Generating 100 tokens..."
+
+for _ <- 1..10 do
+  user_id = Ecto.UUID.generate()
+
+  token = Repo.insert!(%Token{
+    status: "ACTIVE",
+    current_user_id: user_id
+  })
+
+  Repo.insert!(%TokenUsageHistory{
+    token_id: token.id,
+    user_id: user_id,
+    activated_at: DateTime.utc_now() |> DateTime.truncate(:second),
+  })
+end
+
+for _ <- 1..90 do
+  Repo.insert!(%Token{
+    status: "AVAILABLE",
+    current_user_id: nil
+  })
+end
+
+IO.puts "Seeds succeeded!"
+IO.puts "- 10 active tokens with history."
+IO.puts "- 90 available tokens."
