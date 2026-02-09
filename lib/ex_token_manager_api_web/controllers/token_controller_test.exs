b70ccef6887d885b2b565dff8c83b9c8eaebcaf1
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-09 17:57:17 -0300

    add test "POST /tokens/claim when exceed 100 activated tokens should get older one"

--- lib/ex_token_manager_api_web/controllers/token_controller_test.exs
+++ lib/ex_token_manager_api_web/controllers/token_controller_test.exs
@@ -1,4 +1,6 @@
 defmodule ExTokenManagerWeb.TokenControllerTest do
+  import Ecto.Query
+
   use ExTokenManagerApiWeb.ConnCase
 
   alias ExTokenManagerApi.{Tokens, Repo}
@@ -25,4 +27,23 @@ defmodule ExTokenManagerWeb.TokenControllerTest do
     assert length(Tokens.index(%{"status" => "ACTIVE"})) == 1
   end
 
+
+  test "POST /tokens/claim when exceed 100 activated tokens should get older one", %{conn: conn} do
+    user_id = Ecto.UUID.generate()
+    now = DateTime.utc_now() |> DateTime.truncate(:second)
+
+    {_count, _} = Repo.update_all(Token, set: [
+      status: "ACTIVE",
+      current_user_id: user_id,
+      updated_at: now
+    ])
+
+    new_user = Ecto.UUID.generate()
+    conn = post(conn, ~p"/api/tokens/claim", %{"user_id" => new_user})
+
+    assert json_response(conn, :created)
+
+    active_count = Repo.one(from t in Token, where: t.status == "ACTIVE", select: count(t.id))
+    assert active_count == 100
+  end
 end
