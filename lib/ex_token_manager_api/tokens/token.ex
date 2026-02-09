fdc723a34fd33786e6989b2b32294bcdf964765b
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-09 11:09:24 -0300

    add token schema

---
+++ lib/ex_token_manager_api/tokens/token.ex
@@ -0,0 +1,23 @@
+defmodule ExTokenManagerApi.Tokens.Token do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias ExTokenManagerApi.Tokens.History
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  schema "tokens" do
+    field :status, :string, default: "AVAILABLE"
+    field :current_user_id, :binary_id
+
+    has_many :history, History
+    timestamps()
+  end
+
+  def changeset(token, attrs) do
+    token
+    |> cast(attrs, [:status, :current_user_id])
+    |> validate_inclusion(:status, ["AVAILABLE", "ACTIVE"])
+  end
+end
