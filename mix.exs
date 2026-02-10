71cbf740f9af1e3d7e56db4ac0d723a122df53b5
Lucas Assis <lukas.ecomp@gmail.com>
2026-02-10 08:35:10 -0300

    add aliases to run swagger docs after phx.server

--- mix.exs
+++ mix.exs
@@ -80,7 +80,9 @@ defmodule ExTokenManagerApi.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      precommit: ["compile --warnings-as-errors", "deps.unlock --unused", "format", "test"]
+      precommit: ["compile --warnings-as-errors", "deps.unlock --unused", "format", "test"],
+      "phx.server": ["phx.swagger.generate", "phx.server"],
+      s: ["phx.swagger.generate", "phx.server"]
     ]
   end
 end
